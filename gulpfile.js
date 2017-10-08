e4e685f2f56f5c8e2a6ae95888e523dbe33061c4
Golga <golgarud@live.fr>
2017-10-08 20:47:45 +0200

    CORE minify css

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var oyana = require('gulp-oyana');
+var options = {
+	"jsMinPath" : "./jsMin",
+	"jsPath" : "./js",
+	"cssPath" : "./css",
+	"scssPath" : "./scss",
+	"outputStyle" : "compressed"
+}
+oyana( options );
